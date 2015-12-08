d9fc5a0d8855cfc6c75d2f31e5b3bab5ad881a7d
alonerocky <lishoulong@gmail.com>
2015-12-07 23:28:49 -0800

    fetch albums

--- Photo Albums/Photo Albums/PhotoAlbumsViewController.swift
+++ Photo Albums/Photo Albums/PhotoAlbumsViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 import FBSDKLoginKit
+import FBSDKCoreKit
+import FBSDKShareKit
 
 class PhotoAlbumsViewController: UIViewController {
-
+    
     @IBAction func logoutButtonTapped(sender: AnyObject) {
         
         let loginManager = FBSDKLoginManager()
@@ -28,24 +30,43 @@ class PhotoAlbumsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        fetchAlbum()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    func fetchAlbum(){
+        
+        let request =  FBSDKGraphRequest(graphPath: "me/albums", parameters: nil)
+        
+        //        let handler = FBSDKGraphRequestHandler()
+        //(FBSDKGraphRequestConnection!, AnyObject!, NSError!) -> Void
+        
+        request.startWithCompletionHandler({
+            (connection: FBSDKGraphRequestConnection!, result: AnyObject!, error: NSError!) in
+            
+            if error != nil {
+                print(error.localizedDescription)
+            } else {
+                print("\(result)")
+            }
+        });
+    }
+    
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
